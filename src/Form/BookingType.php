b8610119d36385200fa23ae358d0e7a811e37f37
Alexandre Blot <alexandre.blt@outlook.com>
2021-08-30 17:25:28 +0200

    Modification du planning -changement du nom de formulaire -customisation des select en new

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,35 @@ class BookingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('beginAt')
-            ->add('endAt')
-            ->add('title')
+            ->add('beginAt', DateTimeType::class,
+                  [
+                      'label' => 'Date de début',
+                      'placeholder' => '...',
+                      'required' => true,
+                      'widget' => 'choice',
+                      //'format' => 'ddMMy',
+                      'years' => range(date('Y'), date('Y') + 5),
+                      'data' => new \DateTime(),
+                      'attr' => ['class' => 'form-control']
+                  ])
+            ->add('endAt', DateTimeType::class,
+                  [
+                      'label' => 'Date de fin',
+                      'placeholder' => '...',
+                      'required' => false,
+                      'widget' => 'choice',
+                      //'format' => 'ddMMy',
+                      'years' => range(date('Y'), date('Y') + 5),
+                      'data' => new \DateTime(),
+                      'attr' => ['class' => 'form-control']
+                  ])
+            ->add('title', TextType::class,
+                  [
+                      'label'=>'Titre',
+                      //'placeholder' => 'Entrez un nom',
+                      'required' => true,
+                      'attr' => ['class' => 'form-control mb-3']
+                  ])
         ;
     }
 
