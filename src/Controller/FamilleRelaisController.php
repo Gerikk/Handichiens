88b9febe363d2b1959d762fff58667fc93c82b17
Marion <m.declerck37@gmail.com>
2021-08-30 10:06:52 +0200

    Création FamilleRelais Controller

---
+++ src/Controller/FamilleRelaisController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FamilleRelaisController extends AbstractController
+{
+    /**
+     * @Route("/famille/relais", name="famille_relais")
+     */
+    public function index(): Response
+    {
+        return $this->render('famille_relais/index.html.twig', [
+            'controller_name' => 'FamilleRelaisController',
+        ]);
+    }
+}
