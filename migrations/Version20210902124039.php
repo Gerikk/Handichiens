3f1cd51fca3a5ceea0db4c6ad6d94bcaef962ea0
Lewen LEROUX <lewen.leroux@gmail.com>
2021-09-02 14:41:40 +0200

    migration maj

---
+++ migrations/Version20210902124039.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210902124039 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE chien ADD race VARCHAR(255) NOT NULL, ADD formation VARCHAR(255) NOT NULL, ADD resume VARCHAR(255) NOT NULL, CHANGE caractere age VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE chien ADD caractere VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP age, DROP race, DROP formation, DROP resume');
+    }
+}
