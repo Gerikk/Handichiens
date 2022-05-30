44f81cc224e288d4c5d1bbbd5abee8cbadc7f4f6
Mickaël AUGER <auger.mickael37@gmail.com>
2022-05-30 14:41:06 +0200

    [ISSUE_98][WIP] Modification des pages des familles relais
    Suppression du bouton de suppression pour les utilisateurs qui ne sont pas des administrateurs

--- src/Controller/FamilleRelaisController.php
+++ src/Controller/FamilleRelaisController.php
@@ -8,17 +8,22 @@ use App\Entity\User;
 use App\Form\AffectationType;
 use App\Form\ProfilFamilleType;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_EDUCATEUR")
+ */
 class FamilleRelaisController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager){
-        $this -> entityManager = $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -26,7 +31,7 @@ class FamilleRelaisController extends AbstractController
      */
     public function findFamille(EntityManagerInterface $em): Response
     {
-        $repository = $em->getRepository(User::class);
+        $repository        = $em->getRepository(User::class);
         $bookingRepository = $em->getRepository(Booking::class);
 
         $familles = $repository->findAll();
@@ -43,21 +48,21 @@ class FamilleRelaisController extends AbstractController
      */
     public function index(Request $request, EntityManagerInterface $em): Response
     {
-        $id = $request->get('id');
+        $id  = $request->get('id');
         $fam = $this->entityManager->getRepository(User::class)->findById($id);
 
         $repository = $em->getRepository(Booking::class);
-        $bookings = $repository->findAll();
+        $bookings   = $repository->findAll();
 
         $repositoryChien = $em->getRepository(Chien::class);
-        $chiens = $repositoryChien->findAll();
+        $chiens          = $repositoryChien->findAll();
 
 
         return $this->render('famille_relais/profil.html.twig', [
             'controller_name' => 'ProfilFamilleRelaisController',
-            'famille' => $fam,
-            'bookings' => $bookings,
-            'chiens' => $chiens,
+            'famille'         => $fam,
+            'bookings'        => $bookings,
+            'chiens'          => $chiens,
         ]);
     }
 
@@ -69,7 +74,7 @@ class FamilleRelaisController extends AbstractController
         $famille = $this->entityManager->getRepository(User::class)->findById($profil->getId());
 
         return $this->render('famille_relais/show.html.twig', [
-            'famille'=>$famille,
+            'famille' => $famille,
         ]);
     }
 
@@ -78,7 +83,6 @@ class FamilleRelaisController extends AbstractController
      */
     public function editAffect(Request $request, Booking $booking): Response
     {
-
         $form = $this->createForm(AffectationType::class, $booking);
         $form->handleRequest($request);
 
@@ -92,17 +96,18 @@ class FamilleRelaisController extends AbstractController
 
         return $this->renderForm('famille_relais/edit_affect.html.twig', [
             'booking' => $booking,
-            'form' => $form,
+            'form'    => $form,
         ]);
     }
 
     /**
      * @Route("/profil-famille-relais/{id}/edit", name="edit_profil_famille_relais")
      */
-    public function edit(User $profil, Request $request): Response {
+    public function edit(User $profil, Request $request): Response
+    {
         $form = $this->createForm(ProfilFamilleType::class, $profil);
-        $id = $request->get('id');
-        $fam = $this->entityManager->getRepository(User::class)->findById($id);
+        $id   = $request->get('id');
+        $fam  = $this->entityManager->getRepository(User::class)->findById($id);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -116,22 +121,26 @@ class FamilleRelaisController extends AbstractController
             return $this->redirectToRoute('profil_famille_relais', ['id' => $profil->getId()]);
         }
 
-        return $this->render('famille_relais/edit.html.twig',
-                             ['form' => $form->createView(),
-                                 'famille' => $fam,
-                                 'bookings' => $fam,
-                             ],
+        return $this->render(
+            'famille_relais/edit.html.twig',
+            [
+                'form'     => $form->createView(),
+                'famille'  => $fam,
+                'bookings' => $fam,
+            ],
 
         );
     }
+
     /**
+     * @IsGranted("ROLE_ADMIN")
      * @Route("/profil-famille-relais/{id}/suppression", name="delete_profil_famille_relais", methods={"POST", "GET"})
      */
     public function deleteUser(User $family, Request $request): Response
     {
         $id = $request->get('id');
 
-        $em = $this->getDoctrine()->getManager();
+        $em      = $this->getDoctrine()->getManager();
         $usrRepo = $em->getRepository(User::class);
         $em->remove($family);
         $em->flush();
@@ -139,6 +148,5 @@ class FamilleRelaisController extends AbstractController
         $this->addFlash('success', "La suppression a bien été prise en compte.");
 
         return $this->redirectToRoute('famille_relais');
-
     }
 }
